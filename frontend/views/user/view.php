4587377e278e11d386d7e2be79fcfba1a578c5bc
Gravit <gravit.min@ya.ru>
2019-02-28 15:19:22 +0700

    [FEATURE] User CRUID updated 2

--- frontend/views/user/view.php
+++ frontend/views/user/view.php
@@ -6,7 +6,7 @@ use yii\widgets\DetailView;
 /* @var $this yii\web\View */
 /* @var $model common\models\User */
 
-$this->title = $model->id;
+$this->title = $model->username;
 $this->params['breadcrumbs'][] = ['label' => 'Users', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
@@ -16,14 +16,14 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+        <?= Yii::$app->user->can("updateUser") ? Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) : ""?>
+        <?= Yii::$app->user->can("updateUser") ? Html::a('Delete', ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
             'data' => [
                 'confirm' => 'Are you sure you want to delete this item?',
                 'method' => 'post',
             ],
-        ]) ?>
+        ]) : "" ?>
     </p>
 
     <?= DetailView::widget([
