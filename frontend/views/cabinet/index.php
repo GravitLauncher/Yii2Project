cebbdb2c75a60605aabab65f6eec59cfd4722531
Gravit <gravit.min@ya.ru>
2019-05-24 08:24:12 +0700

    [FIX] Загрузка скинов и плащей

--- frontend/views/cabinet/index.php
+++ frontend/views/cabinet/index.php
@@ -3,10 +3,10 @@ use yii\widgets\ActiveForm;
 ?>
 
 <?php $form = ActiveForm::begin(['action' => ['cabinet/uploadskin'],'options' => ['method' => 'post']]) ?>
-<?= $form->field($uploadSkin, 'image')->fileInput() ?>
+<?= $form->field($uploadSkin, 'imageFile')->fileInput() ?>
 <button>Загрузить скин</button>
 <?php ActiveForm::end() ?>
 <?php $form = ActiveForm::begin(['action' => ['cabinet/uploadcloak'],'options' => ['method' => 'post']]) ?>
-<?= $form->field($uploadCloak, 'image')->fileInput() ?>
+<?= $form->field($uploadCloak, 'imageFile')->fileInput() ?>
 <button>Загрузить плащ</button>
 <?php ActiveForm::end() ?>
