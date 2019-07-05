95b4590dc731f6cf7c8f9179a8231d430bee2f26
sasha0552 <admin@sasha0552.ru>
2019-07-06 04:18:03 +0600

    Конфиг лаунчсервера

--- frontend/views/wiki/index.php
+++ frontend/views/wiki/index.php
@@ -46,163 +46,181 @@ $this->params['breadcrumbs'][] = "Wiki";
     Скрипты для systemd будут приведены ниже</p>
 <h2>Настройка лаунчсервера</h2>
 <h3>Базовые параметры</h3>
-<p></p>
+<p>В json нет коментариев. <b>Не пытайтесь копировать этот конфиг себе</b></p>
 <pre class="prettyprint">
 {
-  "legacyPort": 7240,
-  "legacyAddress": "localhost",
-  "legacyBindAddress": "0.0.0.0",
-  "projectName": "TestProjectName", //Имя вашего проекта
-  "mirrors": [
-    "http://mirror.gravitlauncher.ml/", //Зеркало, с которого будет скачиваться assets и client командами downloadAsset/downloadClient
+  "projectName": "MineCraft",      // Название проекта
+  "mirrors": [                     // Зеркала для скачивания шаблонов клиента
     "https://mirror.gravit.pro/"
   ],
-  "binaryName": "Launcher", //Имя jar и exe файла
-  "env": "STD", //Окружение лаунчсервера
-  //DEV - на текущий момент аналогичен DEBUG, но в дальнейшем в этом режиме можно будет видеть особые отладочные сообщения
-  //DEBUG - по умолчанию включен режим отладки
-  //STD - стандартная полтитка. Дебаг включается -Dlauncher.debug=true, stacktrace -Dlauncher.stacktrace=true
-  //PROD - Запрет установки флагов debug и stacktrace. Никакого отладочного вывода вы не получите
+  "binaryName": "Launcher",        // Название выходных jar и exe
+  "copyBinaries": true,            // Копировать jar и exe в updates?
+  "env": "STD",                    // Окружение лаунчсервера
+                                     // DEV - на текущий момент аналогичен DEBUG, но в дальнейшем в этом режиме можно будет видеть особые отладочные сообщения
+                                     // DEBUG - по умолчанию включен режим отладки
+                                     // STD - стандартная полтитка. Дебаг включается -Dlauncher.debug=true, stacktrace -Dlauncher.stacktrace=true
+                                     // PROD - Запрет установки флагов debug и stacktrace. Никакого отладочного вывода вы не получите
   "auth": [
     {
-      "provider": { //AuthProvider, отвечает за проверку логина и пароля
+      "provider": {                // AuthProvider, проверка правильности логина и пароля
         "message": "Настройте authProvider",
-        "type": "accept"
+        "type": "reject"
       },
-      "handler": { //AuthHandler, отвечает за UUID, checkServer и joinServer
+      "handler": {                 // AuthHandler, хранение сессий и uuid
         "type": "memory"
       },
-      "textureProvider": { //textureProvider, отвечает за сикны и плащи
+      "textureProvider": {         // textureProvider, выдача скинов/плащей
+        "type": "request",
         "skinURL": "http://example.com/skins/%username%.png",
-        "cloakURL": "http://example.com/cloaks/%username%.png",
-        "type": "request"
+        "cloakURL": "http://example.com/cloaks/%username%.png"
       },
-      "name": "std", //Имя конфигурации.
-      "isDefault": true //Конфигурация по умолчанию
+      "name": "std",               // Название данной конфигурации auth
+      "displayName": "Default",    // Отображаемое название конфигурации auth
+      "isDefault": true            // Конфигурация по умолчанию?
     }
   ],
-  "protectHandler": { //Защита, отвечает за генерацию токена к нативной библиотеке защиты
-    "type": "none"
+  "protectHandler": {
+    "type": "std"                  // protectHandler, защита, отвечающая за генерацию токена к нативной библиотеке защиты
   },
-  "permissionsHandler": { //PermissionsHandler, отвечает за права аккаунтов: canServer/canAdmin/canBot
-    "filename": "permissions.json",
-    "type": "json"
+  "permissionsHandler": {          // permissionsHandler, проверка разрешений администраторов, серверов
+    "type": "json",
+    "filename": "permissions.json"
   },
-  "hwidHandler": { //HWIDHandler, отвечает за бан по HWID
+  "hwidHandler": {                 // hwidHandler, отвечает за бан по HWID
     "type": "accept"
   },
-  "components": { //Компоненты, которые можно включить или выключить. Так же здесь настраиваются некоторые модули
-    "authLimiter": { //Лимит запросов на авторизацию
-      "rateLimit": 3,
-      "rateLimitMilis": 8000,
-      "message": "Превышен лимит авторизаций",
-      "excludeIps": [],
-      "component": "authLimiter"
+  "components": {                  // Компоненты, которые можно включить или выключить. Так же здесь настраиваются некоторые модули
+    "regLimiter": {                            // Ограничение регистраций
+      "type": "regLimiter",                    // Тип
+      "rateLimit": 3,                          // Максимальное количество
+      "rateLimitMilis": 36000000,              // На какое время, в миллисекундах (10 часов)
+      "message": "Превышен лимит регистраций", // Сообщение
+      "excludeIps": []                         // IP адреса, на которые ограничение не действует.
+    },
+    "authLimiter": {                           // Ограничение попыток авторизации
+      "type": "authLimiter",                   // Тип
+      "rateLimit": 3,                          // Максимальное количество
+      "rateLimitMilis": 8000,                  // На какое время, в миллисекундах (8 секунд)
+      "message": "Превышен лимит авторизаций", // Сообщение
+      "excludeIps": []                         // IP адреса, на которые ограничение не действует.
     }
   },
-  "threadCount": 4,
-  "threadCoreCount": 0,
-  "launch4j": {
-    "enabled": false, //Включение Launch4J
-    "productName": "GravitLauncher",
-    "productVer": "5.0.0.0",
-    "fileDesc": "GravitLauncher 5.0.0",
-    "fileVer": "5.0.0.0",
-    "internalName": "Launcher",
-    "copyright": "© GravitLauncher Team",
-    "trademarks": "This product is licensed under GPLv3",
-    "txtFileVersion": "%s, build %d",
-    "txtProductVersion": "%s, build %d"
+  "threadCount": 2,                                        // Не используется
+  "threadCoreCount": 0,                                    // Не используется
+  "launch4j": {                                            // Launch4J (Создание EXE файла)
+    "enabled": true,                                        // Включен?
+    "setMaxVersion": false,                                 // Установить максимальную версию java?
+    "maxVersion": "1.8.999",                                // Максимальная версия java
+    "productName": "GravitLauncher",                        // Название продукта
+    "productVer": "5.0.4.4",                                // Версия продукта
+    "fileDesc": "GravitLauncher 5.0.4",                     // Описание файла
+    "fileVer": "5.0.4.4",                                   // Версия файла
+    "internalName": "Launcher",                             // Внутреннее имя
+    "copyright": "© GravitLauncher Team",                   // Копирайты
+    "trademarks": "This product is licensed under GPLv3",   // Торговые марки
+    "txtFileVersion": "%s, build %d",                       // Как будет выглядеть версия файла
+    "txtProductVersion": "%s, build %d"                     // Как будет выглядеть версия продукта
   },
   "netty": {
-    "clientEnabled": true, //Не используется в 5.0, всегда true
-    "launcherURL": "http://localhost:9274/Launcher.jar", //URL для скачивания jar версии лаунчера
-    "downloadURL": "http://localhost:9274/%dirname%/", //URL для скачивания диреткории dirname
-    "launcherEXEURL": "http://localhost:9274/Launcher.exe", //URL для скачивания exe версии лаунчера
-    "address": "ws://localhost:9274/api", //Адрес API, по которому будет происходить авторизация и весь оставшийся обмен данными
+    "fileServerEnabled": true,                              // Включен ли встроенный файловый сервер?
+    "sendExceptionEnabled": true,                           // При возникновении ошибки в лаунчсервере отправлять текст ошибки лаунчеру для отображения
+    "ipForwarding": false,                                  // Получение IP из заголовка (при использовании nginx/cloudflare)
+    "launcherURL": "http://localhost:9274/Launcher.jar",    // Ссылка на лаунчер JAR
+    "downloadURL": "http://localhost:9274/%dirname%/",      // Ссылка на папки клиента
+    "launcherEXEURL": "http://localhost:9274/Launcher.exe", // Ссылка на лаунчер EXE
+    "address": "ws://localhost:9274/api",                   // Адрес лаунчсервера
+    "bindings": {},                                         // Настройка URL конкретных папок updates. Настройка скачивания zip архивами
     "performance": {
-      "bossThread": 2, //Колличество потоков, принимающих соеденение
-      "workerThread": 8 //Колличество потоков, обрабатывающих запросы
+      "usingEpoll": false,                                  // Оптимизации netty для использования epoll. Повышает производительность. Только Linux
+      "bossThread": 2,                                      // Количество потоков, принимающих соеденение
+      "workerThread": 8                                     // Количество потоков, обрабатывающих запросы
     },
-    "binds": [ //Список адресов для bind
+    "binds": [                                              // Привязки к определенным IP адресам/портам
       {
-        "address": "0.0.0.0",
-        "port": 9274
+        "address": "0.0.0.0",                               // Адрес
+        "port": 9274                                        // Порт
       }
-    ]
+    ],
+    "logLevel": "DEBUG",                                    // Уровень отладочных сообщений netty. Тут можно убрать "Connection reset by peer", ...
+    "proxy": {                                              // Подключение к другому лаунчсерверу
+      "enabled": false,                                     // Включено?
+      "address": "ws://localhost:9275/api",                 // Адрес
+      "login": "login",                                     // Логин
+      "password": "password",                               // Пароль
+      "auth_id": "std",                                     // Тип аутентификации
+      "requests": []                                        // Cписок типов запросов, которые будут проксироваться
+    }
   },
-  "compress": false, //Сжатие файлов при передаче(LEGACY)
-  "whitelistRejectString": "Вас нет в белом списке",
-  "genMappings": false, //Генерация маппингов ProGuard. Помогает понять что пошло не так
-  "isUsingWrapper": false, //Активация врапперов. Необходима для новой нативной защиты
-  "isDownloadJava": false, //Скачивание своей JVM (требуется правка рантайма)
-  "isWarningMissArchJava": true, //Предупреждения о неверной разрядности Java
-  "enabledProGuard": true, //Активировать ProGuard
-  "stripLineNumbers": true, //С помощью ASM убирать номера строк
-  "enableRadon": true, //Включение обфускатора Radon
-  "deleteTempFiles": true, //Удаление временных файлов
-  "enableRcon": false, //Включение удаленного доступа
-  "startScript": "./start.sh" //Скрипт, выполняющийся при команде restart
+  "launcher": {
+    "guardType": "no",                                      // Защита
+    "attachLibraryBeforeProGuard": false                    // Присоеденять библиотеки до обработки ProGuard, а не после
+  },
+  "whitelistRejectString": "Вас нет в белом списке", // Ошибка, когда человека нет в белом списке
+  "genMappings": true,                               // Генерация маппингов ProGuard
+  "isWarningMissArchJava": true,                     // Предупреждения о неверной разрядности Java
+  "enabledProGuard": true,                           // Включение обфускатора ProGuard
+  "enabledRadon": true,                              // Включение обфускатора Radon
+  "stripLineNumbers": true,                          // С помощью ASM убирать номера строк
+  "deleteTempFiles": true,                           // Удаление временных файлов
+  "startScript": "./start.sh"                        // Скрипт, выполняющийся при команде restart
 }
-
 </pre>
-<p>P.S. В json нет коментариев. <b>Не пытайтесь копировать этот конфиг себе</b></p>
 <h2>Команды LaunchServer</h2>
 <p>Существует много команд лаунчсервера, которые можно выполнять из консоли</p>
 <pre class="prettyprint">
-2019.05.09 18:52:16 [INFO] Command 'help'
-2019.05.09 18:52:16 [INFO] Category: basic - Base LaunchServer commands
-2019.05.09 18:52:16 [INFO]  proguarddictregen [nothing] - Regenerates proguard dictonary
-2019.05.09 18:52:16 [INFO]  gc [nothing] - null
-2019.05.09 18:52:16 [INFO]  debug [true/false] [true/false] - null
-2019.05.09 18:52:16 [INFO]  test [nothing] - Test command. Only developer!
-2019.05.09 18:52:16 [INFO]  restart [nothing] - Restart LaunchServer
-2019.05.09 18:52:16 [INFO]  loadmodule [jar] - Module jar file
-2019.05.09 18:52:16 [INFO]  clear [nothing] - Clear terminal
-2019.05.09 18:52:16 [INFO]  version [nothing] - Print LaunchServer version
-2019.05.09 18:52:16 [INFO]  modules [nothing] - get all modules
-2019.05.09 18:52:16 [INFO]  help [command name] - Print command usage
-2019.05.09 18:52:16 [INFO]  stop [nothing] - Stop LaunchServer
-2019.05.09 18:52:16 [INFO]  logconnections [true/false] - Enable or disable logging connections
-2019.05.09 18:52:16 [INFO]  build [nothing] - Build launcher binaries
-2019.05.09 18:52:16 [INFO]  rebind [nothing] - Rebind server socket
-2019.05.09 18:52:16 [INFO]  proguardclean [nothing] - Resets proguard config
-2019.05.09 18:52:16 [INFO]  proguardmappingsremove [nothing] - Removes proguard mappings (if you want to gen new mappings).
-2019.05.09 18:52:16 [INFO] Category: updates - Update and Sync Management
-2019.05.09 18:52:16 [INFO]  downloadclient [version] [dir] - Download client dir
-2019.05.09 18:52:16 [INFO]  syncprofiles [nothing] - Resync profiles dir
-2019.05.09 18:52:16 [INFO]  downloadasset [version] [dir] - Download asset dir
-2019.05.09 18:52:16 [INFO]  unindexasset [dir] [index] [output-dir] - Unindex asset dir (1.7.10+)
-2019.05.09 18:52:16 [INFO]  syncbinaries [nothing] - Resync launcher binaries
-2019.05.09 18:52:16 [INFO]  indexasset [dir] [index] [output-dir] - Index asset dir (1.7.10+)
-2019.05.09 18:52:16 [INFO]  syncupdates [subdirs...] - Resync updates dir
-2019.05.09 18:52:16 [INFO] Category: auth - User Management
-2019.05.09 18:52:16 [INFO]  uuidtousername [uuid] [auth_id] - Convert player UUID to username
-2019.05.09 18:52:16 [INFO]  auth [login] [password] [auth_id] - Try to auth with specified login and password
-2019.05.09 18:52:16 [INFO]  ban [username] - Ban username for HWID
-2019.05.09 18:52:16 [INFO]  gethwid [username] - get HWID from username
-2019.05.09 18:52:16 [INFO]  usernametouuid [username] [auth_id] - Convert player username to UUID
-2019.05.09 18:52:16 [INFO]  unban [username] - Unban username for HWID
-2019.05.09 18:52:16 [INFO] Category: dump - Dump runtime data
-2019.05.09 18:52:16 [INFO]  dumpentrycache [load/unload] [auth_id] [filename] - Load or unload AuthHandler Entry cache
-2019.05.09 18:52:16 [INFO]  dumpsessions [load/unload] [filename] - Load or unload sessions
-2019.05.09 18:52:16 [INFO] Category: service - Managing LaunchServer Components
-2019.05.09 18:52:16 [INFO]  serverstatus [nothing] - Check server status
-2019.05.09 18:52:16 [INFO]  reloadall  - Reload all provider/handler/module config
-2019.05.09 18:52:16 [INFO]  checkinstall [nothing] - null
-2019.05.09 18:52:16 [INFO]  multi [nothing] - null
-2019.05.09 18:52:16 [INFO]  getpermissions [username] - print username permissions
-2019.05.09 18:52:16 [INFO]  reload [name] - Reload provider/handler/module config
-2019.05.09 18:52:16 [INFO]  confighelp [name] - print help for config command
-2019.05.09 18:52:16 [INFO]  getmodulus [nothing] - null
-2019.05.09 18:52:16 [INFO]  givepermission [username] [permission] [true/false] - give permissions
-2019.05.09 18:52:16 [INFO]  component [action] [component name] [more args] - component manager
-2019.05.09 18:52:16 [INFO]  reloadlist  - print reloadable configs
-2019.05.09 18:52:16 [INFO]  configlist [name] - print help for config command
-2019.05.09 18:52:16 [INFO]  config [name] [action] [more args] - call reconfigurable action
-2019.05.09 18:52:16 [INFO] Category: Base
-2019.05.09 18:52:16 [INFO]  scriptmappings [nothing] - null
-2019.05.09 18:52:16 [INFO]  eval [line] - Eval javascript in server script engine
+Category: basic - Base LaunchServer commands
+ proguarddictregen [nothing] - Regenerates proguard dictonary
+ gc [nothing] - null
+ debug [true/false] [true/false] - null
+ test [nothing] - Test command. Only developer!
+ restart [nothing] - Restart LaunchServer
+ loadmodule [jar] - Module jar file
+ clear [nothing] - Clear terminal
+ version [nothing] - Print LaunchServer version
+ modules [nothing] - get all modules
+ help [command name] - Print command usage
+ stop [nothing] - Stop LaunchServer
+ build [nothing] - Build launcher binaries
+ proguardclean [nothing] - Resets proguard config
+ proguardmappingsremove [nothing] - Removes proguard mappings (if you want to gen new mappings).
+Category: updates - Update and Sync Management
+ downloadclient [version] [dir] - Download client dir
+ syncprofiles [nothing] - Resync profiles dir
+ downloadasset [version] [dir] - Download asset dir
+ unindexasset [dir] [index] [output-dir] - Unindex asset dir (1.7.10+)
+ syncbinaries [nothing] - Resync launcher binaries
+ indexasset [dir] [index] [output-dir] - Index asset dir (1.7.10+)
+ syncupdates [subdirs...] - Resync updates dir
+Category: DAO - Data Management
+ setuserpassword [username] [new password] - Set user password
+ getuser [username] - get user information
+ getallusers  - get all users information
+ register [login] [password] - Register new user
+Category: auth - User Management
+ uuidtousername <uuid> <auth_id> - Convert player UUID to username
+ auth <login> <password> <auth_id> - Try to auth with specified login and password
+ ban [username] - Ban username for HWID
+ gethwid [username] - get HWID from username
+ usernametouuid <username> <auth_id> - Convert player username to UUID
+ unban [username] - Unban username for HWID
+Category: dump - Dump runtime data
+ dumpentrycache [load/unload] [auth_id] [filename] - Load or unload AuthHandler Entry cache
+ dumpsessions [load/unload] [filename] - Load or unload sessions
+Category: service - Managing LaunchServer Components
+ clients [nothing] - Show all connected clients
+ serverstatus [nothing] - Check server status
+ reloadall  - Reload all provider/handler/module config
+ checkinstall [nothing] - null
+ multi [nothing] - null
+ getpermissions [username] - print username permissions
+ reload [name] - Reload provider/handler/module config
+ confighelp [name] - print help for config command
+ getmodulus [nothing] - null
+ givepermission [username] [permission] [true/false] - give permissions
+ component [action] [component name] [more args] - component manager
+ reloadlist  - print reloadable configs
+ configlist [name] - print help for config command
+ config [name] [action] [more args] - call reconfigurable action
 </pre>
 <h3>Команды Launcher. Разблокировка консоли. Удаленное управление</h3>
 <p>Начиная с 5.0.0 в лаунчере появилась консоль, которую можно открыть после авторизации при клике справа на значек консоли.<br>
