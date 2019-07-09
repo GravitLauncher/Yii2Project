27fe6519e78d66bab6bc506777dc5fcb0790a064
sasha0552 <admin@sasha0552.ru>
2019-07-09 06:45:10 +0600

    Почти готовы AuthProviderы

--- frontend/views/wiki/authprovider.php
+++ frontend/views/wiki/authprovider.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: gravit
- * Date: 23.02.19
- * Time: 19:40
- */
 
 /* @var $this yii\web\View */
 
@@ -13,104 +7,133 @@ $this->params['breadcrumbs'][] = "AuthProvider";
 ?>
 
 <h2>Настройка AuthProvider</h2>
-<h3>Способ reject</h3>
-<p>Отклоняет любые пары логин-пароль</p>
-<pre class="prettyprint">
- "authProvider": [
-     {
-         "type": "reject",
-         "message": "Ведутся технические работы, приходите позже."
-     }
- ]
- </pre>
 <h3>Способ accept</h3>
 <p>Принимает любые пары логин-пароль</p>
 <pre class="prettyprint">
- "authProvider": [
-     {
-         "type": "accept"
-     }
- ]
-</pre>
-<h3>Способ request</h3>
-<p>Для проверки логина и пароля лаунчсервер обращается к сайту по протоколу HTTP/HTTPS</p>
-<pre class="prettyprint">
-"authProvider": [
-     {
-       "url": "http://gravitlauncher.ml/auth.php?username=%login%&password=%password%",
-       "response": "OK:(?&lt;username&gt;.+)", // Маска ответа
-       "type": "request"
-     }
+"auth": [
+  {
+    "provider": {
+      "type": "accept"
+    }
+  }
 ]
 </pre>
-<p>Настройка permissions этим способом</p>
+<h3>Способ reject</h3>
+<p>Отклоняет любые пары логин-пароль<br>
+message - сообщение при авторизации</p>
 <pre class="prettyprint">
-"authProvider": [
-     {
-       "url": "http://gravitlauncher.ml/auth.php?username=%login%&password=%password%",
-       "usePermission": true,
-       "response": "OK:(?&lt;username&gt;.+):(?&lt;permissions&gt;.+)", // Маска ответа
-       "type": "request"
-     }
+"auth": [
+  {
+    "provider": {
+      "type": "reject",
+      "message": "Ведутся технические работы, приходите позже"
+    }
+  }
 ]
 </pre>
 <h3>Способ mysql</h3>
 <p>Для проверки логина-пароля лаунчсервер обращается к базе данных mysql<br>
-    Этот способ НЕ подходит для сайтов с нестандартнми алгоритмами хеширования</p>
+Этот способ НЕ подходит для сайтов с нестандартными алгоритмами хеширования (поддерживается только md5)<br>
+mySQLHolder<br>
+  address - адрес mysql сервера<br>
+  port - порт mysql сервера<br>
+  username - имя пользователя на сервере mysql<br>
+  password - пароль пользователя<br>
+  database - база данных (до ?), после находится установка серверной таймзоны<br>
+  timezone - установка клиентской таймзоны<br>
+query - sql запрос, ? по порядку заменяются параметрами из queryParams (перевод: "ВЫБРАТЬ логин ИЗ ТАБЛИЦЫ users ГДЕ ЛОГИН=? И пароль=MD5(?) МАКСИМУМ 1")<br>
+queryParams - параметры sql запроса<br>
+message - сообщение при неверном пароле</p>
 <pre class="prettyprint">
- "authProvider": [
- {
-     "type": "mysql",
-     "mySQLHolder": {
-         "address": "localhost",
-         "port": 3306,
-         "username": "launchserver",
-         "password": "password",
-         "database": "db",
-         "timezone": "UTC"
-     },
-     "query": "SELECT name FROM dle_users WHERE (email=? OR name=?) AND password=MD5(MD5(?)) LIMIT 1",
-     "queryParams": [ "%login%", "%login%", "%password%" ],
-     "message": "Пароль неверный!"
- }
+"auth": [
+  {
+    "provider": {
+      "type": "mysql",
+      "mySQLHolder": {
+        "address": "localhost",
+        "port": 3306,
+        "username": "launchserver",
+        "password": "password",
+        "database": "db?serverTimezone=UTC",
+        "timezone": "UTC"
+      },
+      "query": "SELECT login FROM users WHERE login=? AND password=MD5(?) LIMIT 1",
+      "queryParams": [ "%login%", "%password%" ],
+      "message": "Пароль неверный!"
+    }
+  }
 ]
 </pre>
 <p>Настройка permnissions этим спосбом</p>
 <pre class="prettyprint">
- "authProvider": [
- {
-     "type": "mysql",
-     "mySQLHolder": {
-         "address": "localhost",
-         "port": 3306,
-         "username": "launchserver",
-         "password": "password",
-         "database": "db",
-         "timezone": "UTC"
-     },
-     "query": "SELECT name,permission FROM dle_users WHERE (email=? OR name=?) AND password=MD5(MD5(?)) LIMIT 1",
-     "queryParams": [ "%login%", "%login%", "%password%" ],
-     "usePermission": true,
-     "message": "Пароль неверный!"
- }
+"auth": [
+  {
+    "provider": {
+      "type": "mysql",
+      "mySQLHolder": {
+        "address": "localhost",
+        "port": 3306,
+        "username": "launchserver",
+        "password": "password",
+        "database": "db?serverTimezone=UTC",
+        "timezone": "UTC"
+      },
+      "query": "SELECT login, <b>permission</b> FROM users WHERE login=? AND password=MD5(?) LIMIT 1",
+      "queryParams": [ "%login%", "%password%" ],
+      "usePermission": true,
+      "message": "Пароль неверный!"
+    }
+  }
+]
+</pre>
+<h3>Способ request</h3>
+<p>Для проверки логина и пароля лаунчсервер обращается к сайту по протоколу HTTP/HTTPS<br>
+url - ссылка до скрипта<br>
+response - маска ответа, если не соответствует, будет выведено сообщение с возвращенным текстом</p>
+<pre class="prettyprint">
+"auth": [
+  {
+    "provider": {
+      "type": "request",
+      "url": "http://gravit.pro/auth.php?username=%login%&password=%password%&ip=%ip%",
+      "response": "OK:(?&lt;username&gt;.+)"
+    }
+  }
+]
+</pre>
+<p>Настройка permissions этим способом</p>
+<pre class="prettyprint">
+"auth": [
+  {
+    "provider": {
+      "type": "request",
+      "usePermission": true,
+      "url": "http://gravit.pro/auth.php?username=%login%&password=%password%&ip=%ip%",
+      "response": "OK:(?&lt;username&gt;.+):(?&lt;permissions&gt;.+)"
+    }
+  }
 ]
 </pre>
 <h3>Способ json</h3>
-<p>Для проверки логина-пароля лаунчсервер обращается к сайту по протоколу HTTP/HTTPS, но в отличии от request делает POST запрос с json данными внутри</p>
+<p>Для проверки логина-пароля лаунчсервер обращается к сайту по протоколу HTTP/HTTPS, но в отличии от request делает POST запрос с json данными внутри<br>
+url - ссылка до скрипта<br>
+apiKey - секретный ключ, который может проверятся в скрипте, для безопасности</p>
 <pre class="prettyprint">
-"authProvider": [
- {
-     "type": "json",
-     "url": "http://gravitlauncher.ml/auth.php",
-     "apiKey": "none" //Любая строка, передается в каждом запросе
- }
+"auth": [
+  {
+    "provider": {
+      "type": "json",
+      "url": "http://gravit.pro/auth.php",
+      "apiKey": "none"
+    }
+  }
 ]
 </pre>
-<p>При этом способе настройка permissions не выполняется, так как по умолчанию сервер обязан передавать permissions
-    <br>Запрос:</p>
+<p>При этом способе настройка permissions не выполняется, так как по умолчанию сервер обязан передавать permissions<br>
+Запрос:</p>
 <pre class="prettyprint">
 {
-    "username": "Admin",
+    "username": "admin",
     "password": "password",
     "ip": "127.0.0.1",
     "apiKey": "none"
@@ -119,8 +142,8 @@ $this->params['breadcrumbs'][] = "AuthProvider";
 <p>Ответ:</p>
 <pre class="prettyprint">
 {
-    "username": "Admin",
-    "permissions": 0 //Маска привилегий
+    "username": "admin",
+    "permissions": 0
 }
 </pre>
 <p>Ошибка:</p>
@@ -129,13 +152,5 @@ $this->params['breadcrumbs'][] = "AuthProvider";
     "error": "Неверный логин или пароль"
 }
 </pre>
-<h3>Способ mojang</h3>
-<p><b>Начиная с 5.0 этот способ вынесен в модуль LegacySupport</b></p>
-<p>Выполняет запрос к веб серверам mojang для проверки логина-пароля</p>
-<pre class="prettyprint">
-"authProvider": [
- {
-     "type": "mojang"
- }
-]
-</pre>
+<h3>Способ hibernate</h3>
+<p>TODO</p>
