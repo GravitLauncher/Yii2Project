ba581bede8be61fc40828a4129f573ee094f267a
sasha0552 <admin@sasha0552.ru>
2019-07-20 18:55:25 +0600

    hwidhandler сделал (не все способы)

--- frontend/views/wiki/hwidhandler.php
+++ frontend/views/wiki/hwidhandler.php
@@ -1,20 +1,17 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: gravit
- * Date: 23.02.19
- * Time: 19:49
- */
+
+/* @var $this yii\web\View */
+
 $this->title = 'HWIDHandler - Wiki - GravitLauncher';
 $this->params['breadcrumbs'][] = "HWIDHandler";
 ?>
 
 <h2>Настройка HWIDHandler</h2>
 <h3>Способ accept</h3>
-<p>Принимает любые hwid. Баны недоступны</p>
+<p>Принимает любые hwid и никуда их не записывает. Банить не получится</p>
 <pre class="prettyprint">
 "hwidHandler": {
-     "type": "accept"
+    "type": "accept"
 }
 </pre>
 <h3>Способ mysql</h3>
@@ -24,38 +21,47 @@ $this->params['breadcrumbs'][] = "HWIDHandler";
      "type": "mysql",
 
      "mySQLHolder": {
-       "address": "localhost",
-       "port": 3306,
-       "username": "launchserver",
-       "password": "password",
-       "database": "db"
-     },
+        "address": "localhost",              // адрес mysql сервера
+        "port": 3306,                        // порт mysql сервера
+        "username": "launchserver",          // имя пользователя
+        "password": "password",              // пароль пользователя
+        "database": "db?serverTimezone=UTC", // база данных (до ?), после находится установка серверной таймзоны
+        "timezone": "UTC"                    // установка клиентской таймзоны
+      },
 
-     "queryHwids": "SELECT * FROM `users_hwids` WHERE `totalMemory` = ? and `serialNumber` = ? and `HWDiskSerial` = ? and `processorID` = ?",
-     "paramsHwids": [ "%totalMemory%", "%serialNumber%", "%HWDiskSerial%", "%processorID%" ],
+     "queryHwids": "SELECT * FROM `users_hwids` WHERE `totalMemory` = ? and `serialNumber` = ? and `HWDiskSerial` = ? and `processorID` = ? and `macAddr` = ?",           // sql запрос, ? по порядку заменяются параметрами из paramsHwids
+     "paramsHwids": [ "%totalMemory%", "%serialNumber%", "%HWDiskSerial%", "%processorID%", "%macAddr%" ],                                                                // параметры sql запроса
 
-     "queryBan": "UPDATE `users_hwids` SET `isBanned` = ? WHERE `totalMemory` = ? and `serialNumber` = ? and `HWDiskSerial` = ? and `processorID` = ?",
-     "paramsBan": [ "%isBanned%", "%totalMemory%", "%serialNumber%", "%HWDiskSerial%", "%processorID%" ],
+     "queryBan": "UPDATE `users_hwids` SET `isBanned` = ? WHERE `totalMemory` = ? and `serialNumber` = ? and `HWDiskSerial` = ? and `processorID` = ? and `macAddr` = ?", // sql запрос, ? по порядку заменяются параметрами из paramsBan
+     "paramsBan": [ "%isBanned%", "%totalMemory%", "%serialNumber%", "%HWDiskSerial%", "%processorID%", "%macAddr%" ],                                                    // параметры sql запроса
 
-     "tableUsers": "users",
-     "tableHwids": "users_hwids",
+     "tableUsers": "users",                    // таблица с пользователями
 
-     "userFieldHwid": "hwid",
-     "userFieldLogin": "username",
+     "userFieldHwid": "hwid",                  // название столбца с ID пользователя в таблице с HWIDами
+     "userFieldLogin": "username",             // название столбца с именами пользователей
 
-     "hwidFieldBanned": "isBanned",
-     "hwidFieldTotalMemory": "totalMemory",
-     "hwidFieldSerialNumber": "serialNumber",
-     "hwidFieldHWDiskSerial": "HWDiskSerial",
-     "hwidFieldProcessorID": "processorID",
+     "tableHwids": "users_hwids",              // таблица с hwid
 
-     "banMessage": "Ваш аккаунт заблокирован!"
+     "hwidFieldBanned": "isBanned",            // название столбца с значением, забанен ли пользователь
+     "hwidFieldTotalMemory": "totalMemory",    // название столбца с количеством ОЗУ пользователя (в байтах)
+     "hwidFieldSerialNumber": "serialNumber",  // название столбца с серийным номером компьютера пользователя
+     "hwidFieldHWDiskSerial": "HWDiskSerial",  // название столбца с серийным номером жесткого диска пользователя
+     "hwidFieldProcessorID": "processorID",    // название столбца с ID процессора пользователя
+     "hwidFieldMacAddr": "macAddr",            // название столбца с MAC адресом сетевой карты пользователя
+
+     "banMessage": "Ваш аккаунт заблокирован!" // сообщение, когда забаненный пользователь пытается войти
 }
 </pre>
-<p>Есть 2 таблицы - users и users_hwids. В первой таблице в поле hwid хранится id записи в 2 таблице, во второй таблице - характеристики машины пользователя</p>
-<p>Создать необходимые поля и таблицы можно запросом</p>
+
+<p>В таблице users хранятся данные о пользователях (минимум: ник), в столбце "hwid" хранится id записи в таблице users_hwids<br>
+В users_hwids хранятся характеристики компьютеров пользователей</p>
+<p>Создать необходимый столбец и таблицу можно запросом</p>
 <pre class="prettyprint">
+-- Добавляет столбец "hwid", для связи таблицы с users_hwids
+-- Замените users на название таблицы
 ALTER TABLE `users` ADD `hwid` BIGINT NOT NULL;
+
+-- Создаёт таблицу для хранения характеристик компьютеров пользователей
 CREATE TABLE users_hwids (
  id BIGINT(20) NOT NULL AUTO_INCREMENT,
  isBanned tinyint(1) NOT NULL DEFAULT '0',
@@ -63,6 +69,7 @@ CREATE TABLE users_hwids (
  serialNumber text NOT NULL,
  HWDiskSerial text NOT NULL,
  processorID text NOT NULL,
+ macAddr text NOT NULL,
  PRIMARY KEY (id)
 )
 </pre>
