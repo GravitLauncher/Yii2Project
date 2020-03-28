8f1162b434d413204812a9ee170a21c2c113e4df
Gravit <gravit.min@ya.ru>
2020-03-28 08:24:52 +0700

    [FEATURE] More teatures 5.1+

--- frontend/views/wiki/hibernate.php
+++ frontend/views/wiki/hibernate.php
@@ -23,3 +23,16 @@ $this->params['breadcrumbs'][] = "Hibernate";
 }
 </pre>
 </p>
+<p>Иногда Hibernate просит указать SQL диалект для некоторых драйверов. Список стандартных диалектов можно посмотреть <a href="https://docs.jboss.org/hibernate/orm/3.5/api/org/hibernate/dialect/package-summary.html">тут</a>. Некоторый неполный список драйверов, с которыми работает Hibernate:</p>
+<ul>
+<li>org.hsqldb.jdbcDriver(URL jdbc:hsqldb:hsql://localhost, Dialect org.hibernate.dialect.HSQLDialect) - HSQL - встроеная база данных в оперативной памяти. Только для тестирования</li>
+<li>org.postgresql.Driver(URL jdbc:postgresql://localhost/launchserver, Dialect org.hibernate.dialect.PostgreSQLDialect) - PostgreSQL - конфигурация по умолчанию</li>
+<li>com.mysql.jdbc.Driver (URL jdbc:mysql://localhost/launchserver, Dialect org.hibernate.dialect.MySQLDialect) - MySQL(MariaDB/PersonaServer)</li>
+</ul>
+<h3>Автосоздание таблиц</h3>
+<p>У Hibernate есть возможность автоматически создать необходимые таблицы для своей работы для любой поддерживаемой БД. После настройки параметров подключения запустите лаунчсервер с опцией JVM <span class="codes">-Dhibernate.hbm2ddl.auto=ЗНАЧЕНИЕ</span>, где выберите одно из следующих значений:</p>
+<ul>
+<li>validate - только проверка, никаких изменений в базу не будет внесено</li>
+<li>update - обновление структуры таблиц без потери данных</li>
+<li>create - удаление всех данных из БД и повторное создание таблиц</li>
+</ul>
