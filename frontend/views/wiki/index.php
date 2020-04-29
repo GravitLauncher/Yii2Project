240f9dfcc128a9aabcb2d60cac1b05b3af839a0d
Gravit <gravit.min@ya.ru>
2020-04-30 06:51:08 +0700

    [FIX] Remove depreacted

--- frontend/views/wiki/index.php
+++ frontend/views/wiki/index.php
@@ -239,9 +239,10 @@ $this->params['breadcrumbs'][] = "Wiki";
 <h2><a name="runtime_guide"></a>Сборка лаунчера на 5.1.0+ <div class="gtag gtag-important">Важно</div></h2>
 <p>Если вы соберете лаунчер командой build сразу после настройки лаунчсервера - вы можете получить ошибку "GUI часть лаунчера не найдена"</p>
 <p>Это происходит потому, что начиная с 5.1.0 рантайм(GUI часть лаунчера) отделена от самого лаунчера и находится в отдельном репозитории.<br>
-<b>Ссылка на репозиторий рантайма</b>: <a href="https://github.com/GravitLauncher/LauncherRuntime">GitHub</a> <a href="https://gitlab.com/gravitlauncherteam/launcherjavaruntime">GitLab (УСТАРЕВШЕЕ)</a><br>
+<b>Ссылка на репозиторий рантайма</b>: <a href="https://github.com/GravitLauncher/LauncherRuntime">GitHub</a><br>
 <p><b>Для ветки dev лаунчсервера используется ветка dev рантайма. Для ветки master лаунчесрвера используется ветка master рантайма</b></p>
-<b>Ссылка на CI рантайма</b>: <a href="https://github.com/GravitLauncher/LauncherRuntime/actions">GitHub Actions</a> <a href="https://gitlab.com/gravitlauncherteam/launcherjavaruntime/pipelines">GitLab Pipelines(УСТАРЕВШЕЕ)</a><br>
+<b>Ссылка на CI рантайма</b>: <a href="https://github.com/GravitLauncher/LauncherRuntime/actions">GitHub Actions</a><br>
+<b>Если вы не можете скачать файлы с GitHub Actions войдите/зарегистрируйтесь на GitHub либо скачайте из раздела релизов</b>
 Инструкция по установке нового рантайма:</p>
 <ol>
 <li>Скачайте последний билд рантайма с GitLab pipelines по ссылкам выше. Вы должны получить runtime.zip, содержащий дизайн и локализацию(fxml/css/png/runtime_**.properties) и JavaRuntime-xxxx.jar, содержащий код отображения этого дизайна и взаимодействия с пользователем.</li>
